c40cbd69dd28f56162c32628604dd66709f8ff08
Shawn Tan <shawntanzk@outlook.com>
2026-01-17 17:40:25 +0800

    Remove sitemap integration - fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.signamind.com',
-  integrations: [sitemap()]
+  site: 'https://www.signamind.com'
 });
